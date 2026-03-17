98117d2ae7035326b8684b26d560d9eec9bee8a0
yutaiyes <46273711+yutaiyes@users.noreply.github.com>
2026-03-17 12:50:47 +0800

    Update init.ts
    Signed-off-by: yutaiyes <46273711+yutaiyes@users.noreply.github.com>

--- backend/src/modules/init.ts
+++ backend/src/modules/init.ts
@@ -8,8 +8,7 @@ export const initRouter = Router()
 
 // Supabase configuration
 const SUPABASE_URL = 'https://jvyddfsojtsipmiwjcdr.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2eWRkZnNvanRzaXBtaXdqY2RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNDE3NTAsImV4cCI6MjA3NzcxNzc1MH0.BvEXIgONC2GxAUzRIx8I7P3PsTH3p10vh68CEyRPRMg'
-
+const SUPABASE_ANON_KEY = ''
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 // Schema for creating super admin
